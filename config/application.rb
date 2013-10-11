ce75e301422c92ed84c34d44cffba6498944c166
Jon Lemmon <jon@loomio.org>
2013-10-11 18:25:14 +1300

    Install bootstrap 3

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module LoomioMobile
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
+
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
