625ddb37c06596868985924d4bd9c276f86a0798
Jon Lemmon <jon@loomio.org>
2013-10-11 16:04:50 +1300

    Initial commit

---
+++ app/controllers/discussion_controller.rb
@@ -0,0 +1,4 @@
+class DiscussionController < ApplicationController
+  def show
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoomioMobile::Application.config.secret_token = '2919c2981560da0bcb0940bf0a192d783e4d046877648734be49b83a57b2e21dab934c7d2e92316a9398df62d97ffe72d83981111ac460d87794e6f15f293920'
